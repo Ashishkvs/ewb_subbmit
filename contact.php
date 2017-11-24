12eec1dbf34df0b74aab3eea36094a53890e291b
ewb_subbmit <ashishkvs@gmail.com>
2017-11-24 12:35:34 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,51 @@
+﻿<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+  <?php include("menu.php"); ?>
+		<div id="content">
+			<div id="home">
+<div>
+                <h2>Contact Us</h2>
+                <h5>Dr. H. Mohapatra</h5>
+				<p>Head, Department of physics </p>
+				<p>EWB Advisor</p>
+				<p>Tel: +91-9437275893</p>
+				
+				
+				<h5>Dr. B. S. Acharya</h5>
+				<p>Professor, Department of Physics</p>
+				<p>EWB Advisor</p>
+				<p>Tel: +91-9937463281</p>
+				
+				
+				<h5>Dr.  S. S. Mohanty</h5>
+				<p>Professor, Department of Civil Engineering</p>
+				<p>EWB Advisor</p>
+				<p>Tel: +91-9437206204</p>
+
+
+                <h5>Mr. S. C. Nayak</h5>
+				<p>Asst. Professor, Department of Computer Science & Engineering</p>
+				<p>EWB Faculty Co-ordinator</p>
+				<p>Tel: +91-9777960758</p>
+				
+
+                <h5>Mrs. S. Parida</h5>
+				<p>Asst. Professor, Department of Computer Science & Engineering</p>
+				<p>EWB Faculty Co-ordinator</p>
+				<p>Tel: +91-9861665068</p>
+
+
+                <h5>Contact E-Mail Id:</h5>
+                <p>suvendu2006@gmail.com</p>
+                <p>EWBCVRCE2015@gmail.com</p>
+</div>
+
+			</div>
+		</div>
+		<?php include("footer.php"); ?>
+</body>
+</html>
